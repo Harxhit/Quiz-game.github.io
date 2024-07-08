fc639c03dae5a985e36134654516373470f397b8
Harshit Singh <86526868+Harxhit@users.noreply.github.com>
2024-07-08 21:22:29 +0530

    main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ question_bank = []
 for question in question_data :
   question_text = question["text"]
   question_answer = question["answer"]
-  new_question = Question(question["text"], question["answer"])
+  new_question = Question(question_text, question_answer)
   question_bank.append(new_question)
 
 quiz = Brain(question_bank)
